7806ddb43be31c5b8ac3ed839d7b0e6922250b52
JhayD02 <desiertoeljhay@gmail.com>
2025-03-18 23:56:45 +0800

    Add audio management features and volume control functionality

---
+++ AudioManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+public class AudioManager : NetworkBehaviour
+{
+    [SerializeField] GameObject lobbyMusic;
+    [SerializeField] GameObject inGameMusic;
+
+    [SerializeField] AudioSource soundEffects;
+    public AudioClip punchSound;
+    public AudioClip shootSound;
+    public AudioClip jumpSound;
+    public AudioClip dashSound;
+
+    [SerializeField] AudioSource walkSoundEffects;
+
+    void Start()
+    {
+        // No need to add listener here, as volume is controlled by VolumeChanger
+    }
+
+    public static void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+    }
+
+    public override void OnStartClient()
+    {
+        lobbyMusic.SetActive(false);
+        inGameMusic.SetActive(true);
+        base.OnStartClient();
+
+        if (!isLocalPlayer) return; // Ensure only the local player controls music
+    }
+
+    public override void OnStopClient()
+    {
+        inGameMusic.SetActive(false);
+        lobbyMusic.SetActive(true);
+
+        base.OnStopClient();
+
+        if (!isLocalPlayer) return; // Ensure only the local player controls music
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        soundEffects.PlayOneShot(clip);
+    }
+
+    public void PlayWalkSound()
+    {
+        walkSoundEffects.Play();
+    }
+
+    public void StopWalkSound()
+    {
+        walkSoundEffects.Stop();
+    }
+}
